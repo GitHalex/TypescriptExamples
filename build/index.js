b901b2f26565d6b6bd2503189b4e45cd3b1f14c6
GitHalex <calex1731@gmail.com>
2024-08-03 15:34:43 -0400

    Ejercicio dodne ingresamos un numero donde agregamos un numero

--- build/index.js
+++ build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hola");
+// almacenamiento del nombre en la variable name
+let name = "Alex";
+console.log(name);
+let last_name = "Calcina";
+console.log(last_name);
+let full_name = (name, last_name) => {
+    console.log(`${name} es mi nombre  y mi apellido es ${last_name}`);
+};
+full_name(name, last_name);
+// Declaracion de Variables
+let nombre = "Martin";
+console.log("Hola " + nombre);
+console.log(`Hola ${nombre}`);
+let edad = "30";
+console.log(typeof edad);
+const PI = 3.1416;
+console.log(PI);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log("hola");
-console.log("perri");
+// almacenamiento del nombre en la variable name
+let name: String = "Alex";
+console.log(name);
+let last_name: String = "Calcina";
+console.log(last_name);
+let full_name = (name: String, last_name: String) => {
+  console.log(`${name} es mi nombre  y mi apellido es ${last_name}`);
+};
+full_name(name, last_name);
+
+// Declaracion de Variables
+let nombre: String = "Martin";
+console.log("Hola " + nombre);
+console.log(`Hola ${nombre}`);
+
+let edad: String = "30";
+console.log(typeof edad);
+
+const PI: number = 3.1416;
+console.log(PI);
